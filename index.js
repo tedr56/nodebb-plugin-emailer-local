c260b0aadf0a9f6a5b19a8a6f03ac362d3c12ef6
barisusakli <barisusakli@gmail.com>
2015-10-04 19:17:28 -0400

    closes #8

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-var fs = require('fs'),
-    path = require('path'),
+'use strict';
 
-    winston = module.parent.require('winston'),
+var winston = module.parent.require('winston'),
     Meta = module.parent.require('./meta'),
 
     nodemailer = require('nodemailer'),
     Emailer = {};
 
 
+var settings = {};
+
 Emailer.init = function(data, callback) {
-    function renderAdminPage(req, res, next) {
+    function renderAdminPage(req, res) {
         res.render('admin/emailers/local', {});
     }
 
     data.router.get('/admin/emailers/local', data.middleware.admin.buildHeader, renderAdminPage);
     data.router.get('/api/admin/emailers/local', renderAdminPage);
 
+    Meta.settings.get('emailer-local', function(err, _settings) {
+        if (err) {
+            return winston.error(err);
+        }
+        settings = _settings;
+    });
+
     callback();
 };
 
 Emailer.send = function(data, callback) {
-    var username = Meta.config['emailer:local:username'];
-    var pass = Meta.config['emailer:local:password'];
+
+    var username = settings['emailer:local:username'];
+    var pass = settings['emailer:local:password'];
     var transportOptions = {
-        host: Meta.config['emailer:local:host'],
-        port: Meta.config['emailer:local:port']
+        host: settings['emailer:local:host'],
+        port: settings['emailer:local:port']
     };
     if( username || pass ) {
         transportOptions.auth = {
@@ -40,16 +49,15 @@ Emailer.send = function(data, callback) {
         html: data.html,
         text: data.plaintext,
         subject: data.subject
-    },function(err,response) {
+    },function(err) {
         if ( !err ) {
             winston.info('[emailer.smtp] Sent `' + data.template + '` email to uid ' + data.uid);
         } else {
             winston.warn('[emailer.smtp] Unable to send `' + data.template + '` email to uid ' + data.uid + '!!');
-            // winston.error('[emailer.smtp] ' + response.message);
-        }
+          }
         callback(err, data);
     });
-}
+};
 
 Emailer.admin = {
     menu: function(custom_header, callback) {
